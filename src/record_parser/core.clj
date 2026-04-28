d7a5eb8f08c5f1e91b8d2d37ee8565eb913273f4
kumar88kanishk <kumar88kanishk@gmail.com>
2026-04-28 19:08:11 +0530

    Parse records completed

--- src/record_parser/core.clj
+++ src/record_parser/core.clj
@@ -1,14 +1,44 @@
 (ns record-parser.core
-  (:require [clojure.string as str]
-            [clojure.java.io as io])
+  (:require [clojure.string :as str]
+            [clojure.java.io :as io])
   (:import [java.time.format DateTimeFormatter]
            [java.time LocalDate]))
 
+(def date-formatter (DateTimeFormatter/ofPattern "M/d/yyyy"))
 
-(defn load-records [file delimeter]
-  (with-open [rdr (io/reader file)]
-    (doseq [line (line-seq rdr)]
-      (println line))))
+(def output-date-formatter (DateTimeFormatter/ofPattern "MM/dd/yyyy"))
 
-(def -main [& args]
-  (load-records "input/commas.txt" ","))
+(defn parse-date 
+  [date-str]
+  (-> date-str
+      str/trim
+      (str/replace #"-" "/")
+      (LocalDate/parse date-formatter)))
+
+(defn parse-record 
+  [line delimiter]
+  (let [fields (map str/trim (str/split line delimiter))]
+    (zipmap [:last-name :first-name :gender :color :dob]
+            [(nth fields 0)
+             (nth fields 1)
+             (nth fields 2)
+             (nth fields 3)
+             (parse-date (nth fields 4))])))
+
+(defn load-records [filename delimiter]
+  (with-open [rdr (io/reader filename)]
+    (doall (map #(parse-record % delimiter)
+                (remove str/blank? (line-seq rdr))))))
+
+(defn -main 
+  []
+  (let [comma-records (load-records "input/comma.txt" #",")
+        pipe-records (load-records "input/pipe.txt" #"\|")
+        space-records (load-records "input/space.txt" #"\s+")
+        all-records (concat comma-records pipe-records space-records)]
+    all-records))
+
+(comment
+  (-main)
+  )
+
--- test/record_parser/core_test.clj
+++ test/record_parser/core_test.clj
@@ -1,7 +1,29 @@
 (ns record-parser.core-test
-  (:require [clojure.test :refer :all]
-            [record-parser.core :refer :all]))
+  (:require [clojure.test :refer [deftest is testing]]
+            [record-parser.core :refer [load-records parse-date parse-record]]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest parse-date-trims-and-parses
+  (testing "parse-date handles leading whitespace and date separators"
+    (is (= "1943-02-13" (str (parse-date " 2/13/1943"))))))
+
+(deftest parse-pipe-delimited-record
+  (testing "parse-record parses pipe-delimited records correctly"
+    (let [record (parse-record "Smith | Steve | Male | Red | 3/3/1985" #"\\|")]
+      (is (= "Smith" (:last-name record)))
+      (is (= "Steve" (:first-name record)))
+      (is (= "3/3/1985" (format "%d/%d/%d" (.getMonthValue (:dob record)) (.getDayOfMonth (:dob record)) (.getYear (:dob record))))))))
+
+(deftest parse-space-delimited-record
+  (testing "parse-record parses space-delimited records correctly"
+    (let [record (parse-record "Kuni Hiroshi Male Blue 1/5/1970" #"\s+")]
+      (is (= "Kuni" (:last-name record)))
+      (is (= "Hiroshi" (:first-name record)))
+      (is (= "1/5/1970" (format "%d/%d/%d" (.getMonthValue (:dob record)) (.getDayOfMonth (:dob record)) (.getYear (:dob record))))))))
+
+(deftest load-records-skips-blank-lines
+  (testing "load-records ignores blank lines in input"
+    (let [tmp-file (doto (java.io.File/createTempFile "records" ".txt")
+                     (spit "Smith | Steve | Male | Red | 3/3/1985\n\n"))
+          records (load-records (.getPath tmp-file) #"\|")]
+      (is (= 1 (count records)))
+      (.delete tmp-file))))
